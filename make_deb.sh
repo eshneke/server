82b78c08e2a20b1c3459b676be0caec1b29d2f6c
eshneke <eshneke@gmail.com>
2025-12-11 16:32:48 +0300

    make deb

--- make_deb.sh
+++ make_deb.sh
@@ -1,1 +1,13 @@
-mkdir -p simple-server-1.0/{DEBIAN,usr/bin,usr/lib/systemd/system}
+# Собрать бинарник
+make
+
+# Скопировать файлы
+cp simple-server simple-server-1.0/usr/bin/
+cp simple-server.service simple-server-1.0/usr/lib/systemd/system/
+
+# Установить права
+chmod 755 simple-server-1.0/usr/bin/simple-server
+chmod 644 simple-server-1.0/usr/lib/systemd/system/simple-server.service
+
+# Собрать .deb
+dpkg-deb --build simple-server-1.0
